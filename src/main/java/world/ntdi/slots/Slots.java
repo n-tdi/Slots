1e7bfca1c5fbbcc8e5d7eb41836ead2d460442b6
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-03-24 15:04:57 -0600

    init

---
+++ src/main/java/world/ntdi/slots/Slots.java
@@ -0,0 +1,17 @@
+package world.ntdi.slots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Slots extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
